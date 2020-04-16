55d9ecf0f9e63c9b13453657ddb61de254ae36ee
Kate Protasova <ket9090@gmail.com>
2020-04-16 17:26:34 +0700

    Initial commit

--- Cocktail/NetworkManager.swift
+++ Cocktail/NetworkManager.swift
@@ -1,19 +1,9 @@
 //
-//  ViewController.swift
+//  NetworkManager.swift
 //  Cocktail
 //
 //  Created by Екатерина Протасова on 16.04.2020.
 //  Copyright © 2020 Екатерина Протасова. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
-}
-
+import Foundation
